b0306e9be7e0aec3a0db4bd5709f41a461030a83
Jean Elder <jeanelder.sa@gmail.com>
2017-10-17 16:41:27 -0300

    Pequeno tapinha no visual

--- app/src/main/java/br/com/apecnet/euvisto/IntagActivity.java
+++ app/src/main/java/br/com/apecnet/euvisto/IntagActivity.java
@@ -1,7 +1,18 @@
 package br.com.apecnet.euvisto;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.io.File;
+import java.util.Hashtable;
 
 public class IntagActivity extends AppCompatActivity {
 
@@ -9,5 +20,41 @@ public class IntagActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intag);
+    }
+
+    private void gerarComZXing(String path, String texto){
+        try {
+
+            File myFile = new File(path+"/"+nomeQrCodeGerado+"."+formatoQrCodeGerado);
+            Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix byteMatrix = qrCodeWriter.encode(texto, BarcodeFormat.QR_CODE, 100, 100, hintMap);
+            int CrunchifyWidth = byteMatrix.getWidth();
+            BufferedImage image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
+                    BufferedImage.TYPE_INT_RGB);
+            image.createGraphics();
+
+            Graphics2D graphics = (Graphics2D) image.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+            graphics.setColor(Color.BLACK);
+x
+            for (int i = 0; i < CrunchifyWidth; i++) {
+                for (int j = 0; j < CrunchifyWidth; j++) {
+                    if (byteMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            ImageIO.write(image, formatoQrCodeGerado, myFile);
+            JOptionPane.showMessageDialog(this, "QRCode gerado em: "+myFile.getAbsolutePath());
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 }
